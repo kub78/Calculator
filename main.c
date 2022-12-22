9389eeb48022ec208960cf0b6f584e77e528a1dd
kub78 <2004egorlap@gmail.com>
2022-12-22 23:05:39 +0300

    Update main.c
    Переделал программу. hot fix сделал.

--- main.c
+++ main.c
@@ -3,40 +3,16 @@
 
 int main(){
 	char simb;
-	int NumBin; // 0 its 8 1 its 16 2 its 2
-	printf("Введите знак: ");
-	scanf(" %c", &simb);
-	if(simb == '~'){
-		printf("Введите число: \n");
-		char num3[128];
-		scanf("%s", num3);
-		if (num3[0] != '0' && num3[1] != 'x'){
-			int res = ~ (Atoi_2(num3));
-			DecToBin(res);
-		}
-		if(num3[0] == '0' && num3[1] != 'x'){
-			int x = strlen(num3);
-			memmove(&num3[0], &num3[0 + 1], x);
-			int res;
-			res = ~(Atoi_8(num3));
-			DecToOctal(res);
-		}
-		if(num3[0] == '0' && num3[1] == 'x'){
-			int x = strlen(num3);
-			memmove(&num3[0], &num3[2], x - 1);
-			int res;
-			res = ~(Atoi_16(num3));
-			printf("0x%x",res);
-		}
-	}
-	else if (simb != '~'){
-		printf("Введите два числа: \n");
-		char num1[128];
-		char num2[128];
-		scanf("%s", num1);
-		scanf("%s", num2);
+	int i;
+	char* line = NULL;
+	size_t len = 0;
+	char LineSize = 0;
+	char num1[128];
+	char num2[128];
+	LineSize = getline(&line,&len,stdin);
+	line[LineSize - 1] = '\0';
+	if (sscanf(line,"%s %c %s", num1, &simb, num2) == 3){
 		if (num1[0] != '0' && num1[1] != 'x' && num2[0] != '0' && num2[1] !='x'){
-			NumBin = 2;
 			if(simb == '+'){
 				int res = Atoi_2(num1) + Atoi_2(num2);
 				DecToBin(res);
@@ -67,7 +43,6 @@ int main(){
 			}
 		} 
 		else if (num1[0] == '0' && num1[1] != 'x' && num2[0] == '0' && num2[1] !='x' ){
-			NumBin = 0;
 			int x = strlen(num1);
 			memmove(&num1[0], &num1[0 + 1], x);
 			memmove(&num2[0], &num2[0 + 1], x);
@@ -102,7 +77,6 @@ int main(){
 			}
 		}
 		else if (num1[0] == '0' && num1[1] == 'x' && num2[0] == '0' && num2[1] == 'x'){
-			NumBin = 0;
 			int x = strlen(num1);	
 			memmove(&num1[0], &num1[2], x - 1);
 			memmove(&num2[0], &num2[2], x - 1);
@@ -140,7 +114,32 @@ int main(){
 		}
 		else{
 			printf("Ошибка: системы счисления не совпадают\n ");
+		}
 	}
+	if (sscanf(line,"~%s",num2) == 1){
+		if (num2[0] != '0' && num2[1] != 'x'){
+			int res = ~ (Atoi_2(num2));
+			DecToBin(res);
+		}
+		if(num2[0] == '0' && num2[1] != 'x'){
+			int x = strlen(num2);
+			memmove(&num2[0], &num2[0 + 1], x);
+			int res;
+			res = ~(Atoi_8(num2));
+			DecToOctal(res);
+		}
+		if(num2[0] == '0' && num2[1] == 'x'){
+			int x = strlen(num2);
+			memmove(&num2[0], &num2[2], x - 1);
+			int res;
+			res = ~(Atoi_16(num2));
+			printf("0x%x",res);
+		}
 	}
+	else{
+		printf("Ошибка: Неправильный ввод\n ");
+	}
+
+
 	return 0;
 }
